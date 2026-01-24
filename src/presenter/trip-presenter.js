d396759771d220b61ef9f903f472c11cb10eb5bb
y.koryakina <y.koryakina@g.nsu.ru>
2026-01-25 02:52:23 +0700

    Вынесла ul в отдельный компонент

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -3,6 +3,8 @@ import SortView from '../view/sort-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import PointView from '../view/point-view.js';
 import InfoView from '../view/trip-info-view.js';
+import PointListView from '../view/point-list-view.js';
+
 import { render, RenderPosition } from '../render.js';
 
 export default class TripPresenter {
@@ -10,24 +12,20 @@ export default class TripPresenter {
     this.filtersContainer = document.querySelector('.trip-controls__filters');
     this.eventsContainer = document.querySelector('.trip-events');
     this.mainContainer = document.querySelector('.trip-main');
-    this.pointsListContainer = null;
-  }
-
-  createPointsList() {
-    const listElement = document.createElement('ul');
-    listElement.className = 'trip-events__list';
-    this.eventsContainer.appendChild(listElement);
-    this.pointsListContainer = listElement;
   }
 
   init() {
     render(new InfoView(), this.mainContainer, RenderPosition.AFTERBEGIN);
     render(new FilterView(), this.filtersContainer);
     render(new SortView(), this.eventsContainer, RenderPosition.AFTERBEGIN);
-    this.createPointsList();
-    render(new EditPointView(), this.pointsListContainer);
+
+    const pointListView = new PointListView();
+    render(pointListView, this.eventsContainer);
+    const pointsListContainer = pointListView.getElement();
+
+    render(new EditPointView(), pointsListContainer);
     for (let i = 0; i < 3; i++) {
-      render(new PointView(), this.pointsListContainer);
+      render(new PointView(), pointsListContainer);
     }
   }
 }
