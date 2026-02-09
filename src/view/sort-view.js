a3eee9c5b4e42f5726a9b9e6e9eb0e9e68cf2f96
Yana Koryakina <y.koryakina@g.nsu.ru>
2026-02-09 15:37:54 +0700

    деактивация кнопок event и offers

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -6,7 +6,7 @@ function createSortTemplate() {
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
         <div class="trip-sort__item  trip-sort__item--day">
           <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
-          <label class="trip-sort__btn" for="sort-day" data-sort-type="${SORT_TYPE.DAY}">Day</label>
+          <label class="trip-sort__btn  trip-sort__btn--clickable" for="sort-day" data-sort-type="${SORT_TYPE.DAY}">Day</label>
         </div>
 
         <div class="trip-sort__item  trip-sort__item--event">
@@ -16,12 +16,12 @@ function createSortTemplate() {
 
         <div class="trip-sort__item  trip-sort__item--time">
           <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-          <label class="trip-sort__btn" for="sort-time" data-sort-type="${SORT_TYPE.TIME}">Time</label>
+          <label class="trip-sort__btn  trip-sort__btn--clickable" for="sort-time" data-sort-type="${SORT_TYPE.TIME}">Time</label>
         </div>
 
         <div class="trip-sort__item  trip-sort__item--price">
           <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-          <label class="trip-sort__btn" for="sort-price" data-sort-type="${SORT_TYPE.PRICE}">Price</label>
+          <label class="trip-sort__btn  trip-sort__btn--clickable" for="sort-price" data-sort-type="${SORT_TYPE.PRICE}">Price</label>
         </div>
 
         <div class="trip-sort__item  trip-sort__item--offer">
@@ -39,7 +39,7 @@ export default class SortView extends AbstractView {
     super();
     this.#handleSortChange = onSortChange;
 
-    this.element.querySelectorAll('.trip-sort__btn')
+    this.element.querySelectorAll('.trip-sort__btn--clickable')
       .forEach((input) => {
         input.addEventListener('click', this.#SortHandlerChange);
       });
