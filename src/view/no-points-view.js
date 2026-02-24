715b08d10ef26c31269c3d7af5660d5d11daebc2
Yana Koryakina <y.koryakina@g.nsu.ru>
2026-02-24 17:57:53 +0700

    убрала дублирование кода в no-points-view

--- src/view/no-points-view.js
+++ src/view/no-points-view.js
@@ -1,37 +1,22 @@
 import AbstractView from '../framework/view/abstract-view.js';
 import { FilterType } from '../const.js';
 
+const NoPointsMessages = {
+  [FilterType.ALL]: 'Click New Event to create your first point',
+  [FilterType.FUTURE]: 'There are no future events now',
+  [FilterType.PRESENT]: 'There are no present events now',
+  [FilterType.PAST]: 'There are no past events now'
+};
+
 function createNoPointsTemplate(filter) {
-  switch (filter) {
-    case FilterType.ALL:
-      return `
-        <section class="trip-events">
-          <h2 class="visually-hidden">Trip events</h2>
-          <p class="trip-events__msg">Click New Event to create your first point</p>
-        </section>
-      `;
-    case FilterType.FUTURE:
-      return `
-        <section class="trip-events">
-          <h2 class="visually-hidden">Trip events</h2>
-          <p class="trip-events__msg">There are no future events now</p>
-        </section>
-      `;
-    case FilterType.PRESENT:
-      return `
-        <section class="trip-events">
-          <h2 class="visually-hidden">Trip events</h2>
-          <p class="trip-events__msg">There are no present events now</p>
-        </section>
-      `;
-    case FilterType.PAST:
-      return `
-        <section class="trip-events">
-          <h2 class="visually-hidden">Trip events</h2>
-          <p class="trip-events__msg">There are no past events now</p>
-        </section>
-      `;
-  }
+  const message = NoPointsMessages[filter] || NoPointsMessages[FilterType.ALL];
+
+  return `
+    <section class="trip-events">
+      <h2 class="visually-hidden">Trip events</h2>
+      <p class="trip-events__msg">${message}</p>
+    </section>
+  `;
 }
 
 export default class NoPointsView extends AbstractView {
